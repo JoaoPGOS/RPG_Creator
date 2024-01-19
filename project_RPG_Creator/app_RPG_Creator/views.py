3339e856236ccf7ae9340eaa84ab0c0d868ed169
joaopdr12ee <joaopedrogoncalvesdeoliveirasi@gmail.com>
2024-01-19 19:24:14 -0300

    Cad, login, forgot password, email verification - done

--- project_RPG_Creator/app_RPG_Creator/views.py
+++ project_RPG_Creator/app_RPG_Creator/views.py
@@ -9,9 +9,8 @@ def generate_random_number():
     return str(random.randint(10000000, 99999999))
 
 def send_email(receiver_email, subject, body):
-    sender_email = "thecodeofdavinci@gmail.com"  # Replace with your email
-    sender_password = "qdbf pklb ommj klgj "  # Replace with your email password
-
+    sender_email = "thecodeofdavinci@gmail.com" 
+    sender_password = "qdbf pklb ommj klgj "  
     message = MIMEMultipart()
     message['From'] = sender_email
     message['To'] = receiver_email
@@ -27,23 +26,25 @@ def send_email(receiver_email, subject, body):
 
 
 
-#Django only <----------------------------------------------------------------------------------------------------------------------->
 
 def cadpage(request):
-    return render(request,'users/login.html')
+    return render(request,'users/register.html')
 
 def loginpage(request):
     return render(request, 'users/loginpage.html')
 
 def cad_user(request):
-    exist_user = users.objects.filter(email=request.POST.get('email')).first()
+    user_email = request.POST.get('email')
+    user_password = request.POST.get('password')
+
+    exist_user = users.objects.filter(email=user_email).first()
     if exist_user:
         return render(request, 'users/login.html', {'registered_user': 'Email já cadastrado'})
     else:
         new_user = users()
         random_number = generate_random_number()
-        new_user.email = request.POST.get('email')
-        new_user.password = request.POST.get('password')
+        new_user.email = user_email
+        new_user.password = user_password
         new_user.verified = 0
         new_user.code = random_number
         new_user.save()
@@ -54,28 +55,91 @@ def cad_user(request):
     # users.objects.filter(email='').update(password='') to update a column using django
 
     context = {
-        'email': request.POST.get('email'),
-        'password': request.POST.get('password')
-    } # Receiving the email and passing to the render so it will be on the input inside of the form on the other page
-    email_address = f"{request.POST.get('email')}"  # Replace with the recipient's email address
+        'email': user_email,
+        'password': user_password
+    } 
+    email_address = f"{user_email}" 
     email_subject = "Verificação do email"
     email_body = f"Este é o seu código para verificar o seu email: {random_number}"
     send_email(email_address, email_subject, email_body)
     return render(request,'users/verifyemail.html', context)
 
-def homepage(request):
+def end_verification(request):
+    user_email = request.POST.get('email')
 
-    reference_user = users.objects.filter(email=request.POST.get('email')).first()
+    reference_user = users.objects.filter(email=user_email).first()
 
     if reference_user:
-        if reference_user.verified == 0 and reference_user.password != request.POST.get('password'):
+        if reference_user.verified == 0:
             if int(request.POST.get('verified_code')) == reference_user.code:
-                users.objects.filter(email=request.POST.get('email')).update(verified=1)
+                context = {
+                    'response':'Email verificado C:'
+                }
+                users.objects.filter(email=user_email).update(verified=1)
+                return render(request, 'users/loginpage.html', context)
+            else:
+                context = {
+                    'registered_user':'Código de verificação incorreto, cadastre novamente'
+                }
+                users.objects.filter(email=user_email).delete()
+                return render(request, 'users/register.html', context)
+        else:
+            context = {
+                    'response':'Email já verificado, se esqueceu a senha, clique em "Esqueceu a senha?" para recupera-la'
+            }
+            return render(request, 'users/loginpage.html',context)
+    else:
+        context = {
+                    'registered_user':'Email não cadastrado'
+        }
+        return render(request, 'users/register.html', context)
+
+def homepage(request):
+    user_email = request.POST.get('email')
+    user_password = request.POST.get('password')
+
+    reference_user = users.objects.filter(email=user_email).first()
+
+    if reference_user:
+        if reference_user.email == user_email and reference_user.password == user_password:
+            if reference_user.verified == 1:
                 return render(request,'users/homepage.html')
             else:
-                users.objects.filter(email=request.POST.get('email')).delete()
-                return render(request, 'users/login.html', {'registered_user': 'Código de verificação incorreto, cadastre novamente'})
+                context = {
+                    'registered_user':'Código de verificação incorreto, cadastre novamente'
+                }
+                users.objects.filter(email=user_email).delete()
+                return render(request, 'users/register.html', context)
         else:
-            return render(request,'users/homepage.html')
+            context = {
+                'response':'Senha ou Email errado, digite novamente'
+            }
+            return render(request,'users/loginpage.html',context)
     else:
-        return render(request, 'users/login.html', {'registered_user': 'Email não cadastrado'})
+        return render(request, 'users/register.html', {'registered_user': 'Email não cadastrado'})
+
+def forgot_password(request):
+    return render(request, 'users/forgot_password.html')
+
+def request_to_recover_password(request):
+    user_email = request.POST.get('email')
+    reference_user = users.objects.filter(email=user_email).first()
+
+    if reference_user:
+        if reference_user.verified == 1:
+            send_email(user_email,'Recuperar senha',f'Ola aqui está a sua senha: {reference_user.password}')
+            context = {
+                'response':'Acesse o seu email para recuperar a senha'
+            }
+            return render(request, 'users/loginpage.html', context)
+        else:
+            context = {
+                'response': 'Seu email não foi verificado, refaça o cadastro'
+            }
+            users.objects.filter(email=user_email).delete()
+            return render(request, 'users/forgot_password.html', context)
+    else:
+        context = {
+                'response':'Usuário não cadastrado'
+        }
+        return render(request, 'users/forgot_password.html', context)
--- project_RPG_Creator/project_RPG_Creator/urls.py
+++ project_RPG_Creator/project_RPG_Creator/urls.py
@@ -3,8 +3,13 @@ from app_RPG_Creator import views
 
 urlpatterns = [
     # route, view, reference name
+    path('forgotpassword',views.forgot_password,name='forgot_password'),
+    path('recoverpassword',views.request_to_recover_password, name='change_password'),
+
     path('cadpage',views.cadpage,name='cadpage'),
-    path('cad_user',views.cad_user,name='cad_user'),
+    path('caduser',views.cad_user,name='cad_user'),
+    path('endverification',views.end_verification,name='end_verification'),
+
     path('homepage',views.homepage,name='homepage'),
     path('',views.loginpage,name='loginpage')
 ]
